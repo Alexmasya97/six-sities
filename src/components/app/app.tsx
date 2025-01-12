a69572f2b7a6ce2fbb2478f96e278c8147be8cd8
Alexmasya97 <sasa_97@mail.ru>
2025-01-12 23:08:30 +1000

    module3

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,11 @@
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { AppRoute, AuthorizationStatus } from '../../const';
 import { Main } from '../../pages/main/main';
+import { Login } from '../../pages/login/login';
+import { Favorites } from '../../pages/favorites/favorites';
+import { Property } from '../../pages/property/property';
+import NotFound from '../../pages/notFound/notFound';
+import PrivateRoute from '../private-route/private-route';
 
 
 type AppProps = {
@@ -6,7 +13,36 @@ type AppProps = {
 }
 
 function App({ offersCount }: AppProps): JSX.Element {
-  return <Main offersCount={offersCount} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element={<Main offersCount={offersCount} />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<Login />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={
+            <PrivateRoute
+              authorizationStatus={AuthorizationStatus.NoAuth}
+            >
+              <Favorites />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path={AppRoute.Property}
+          element={<Property />}
+        />
+        <Route path="*" element={<NotFound />} />
+
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
