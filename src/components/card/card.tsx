999555c0d3a0a290f8e693a09981861d6c39994a
Alexmasya97 <sasa_97@mail.ru>
2025-02-04 15:30:28 +1000

    kind of optimization

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,5 +1,6 @@
-import type { Offer } from '../../types/types';
+import { Link } from 'react-router-dom';
 
+import type { Offer } from '../../types/types';
 import { AppRoute } from '../../const';
 import { getStarsWidth } from '../../utils';
 
@@ -77,7 +78,7 @@ const Card = ({
           </div>
         </div>
         <h2 className="place-card__name">
-          <a href={`${AppRoute.Property}/${id}`}>{title}</a>
+          <Link to={`${AppRoute.Property}/${id}`}>{title}</Link>
         </h2>
         <p className="place-card__type">{type}</p>
       </div>
